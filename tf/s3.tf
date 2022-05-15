7c7ce5895d4d075067255838a51fc480e5dcb4d4
Igor Le Masson <igorlemasson@gmail.com>
2022-05-15 19:53:22 -0300

    updated the S3 config

--- tf/s3.tf
+++ tf/s3.tf
@@ -4,6 +4,7 @@ resource "aws_s3_bucket" "data_lake" {
 
 resource "aws_s3_bucket_acl" "s3_acl_data_lake" {
   bucket = aws_s3_bucket.data_lake.id
+   acl = "private"
 }
 
 resource "aws_s3_bucket" "scripts" {
